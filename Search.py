c408a41893dea8556b439019b2740c3fcb116533
Mohammad Hossein <iammhz77@gmail.com>
2019-10-20 18:50:28 +0330

    main body of search class

---
+++ Search.py
@@ -0,0 +1,22 @@
+class Search:
+
+    def __init__(self):
+        pass
+
+    def bfs(self):
+        pass
+
+    def dfs(self):
+        pass
+
+    def ucs(self):
+        pass
+
+    def astar(self):
+        pass
+
+    def dls(self):
+        pass
+
+    def ids(self):
+        pass
