20b868b59f1288593ae88ed3817036427b7beda7
Mohammad Hossein <iammhz77@gmail.com>
2019-10-21 19:57:43 +0330

    after conflicts

--- Puzzle8.py
+++ Puzzle8.py
@@ -1,10 +1,25 @@
+import numpy as np
+
+
+# it is class puzzle8
+
+
 class Puzzle8:
+    goal = [1, 2, 3, 4, 5, 6, 7, 8, 0]
 
-    def __init__(self):
-        pass
+    def __init__(self, data):
+        self.data = data
 
-    def is_goal(self):
-        pass
+    # def getData(self):
+    #     return self.data
 
-    def expand(self):
-        pass
+    def is_goal(self, state):
+        return state == Puzzle8.goal
+
+    def expand(self, state):
+        array = np.array(state)
+
+        array = np.reshape(array, (3, 3))
+
+        # we assume that the 0 in list is the blank space
+        space_index = np.where(array == 0)
