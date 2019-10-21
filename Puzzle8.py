235a4cf4db3d76a8dff32a672cbc5007f9f8b6f3
Mohammad Hossein <iammhz77@gmail.com>
2019-10-21 20:04:42 +0330

    test file

--- Puzzle8.py
+++ Puzzle8.py
@@ -5,7 +5,6 @@ import numpy as np
 
 
 class Puzzle8:
-    goal = [1, 2, 3, 4, 5, 6, 7, 8, 0]
 
     def __init__(self, data):
         self.data = data
@@ -14,7 +13,8 @@ class Puzzle8:
     #     return self.data
 
     def is_goal(self, state):
-        return state == Puzzle8.goal
+        goal = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+        return state == goal
 
     def expand(self, state):
         array = np.array(state)
@@ -23,3 +23,4 @@ class Puzzle8:
 
         # we assume that the 0 in list is the blank space
         space_index = np.where(array == 0)
+        print(space_index)
--- Test.py
+++ Test.py
@@ -1,4 +1,6 @@
+from Puzzle8 import Puzzle8
 
+test = [1, 2, 3, 4, 0, 5, 6, 7, 8]
+p = Puzzle8(test)
 
-
-
+p.expand(test)
