48b2d151c9b24323d679358388a317c5a16749cf
Mohammad Hossein <iammhz77@gmail.com>
2019-10-22 16:03:04 +0330

    utility : is_goal input changed

--- Utility.py
+++ Utility.py
@@ -1,6 +1,6 @@
 # def getData(self):
 #     return self.data
 @staticmethod
-def is_goal(puzzle):
+def is_goal(data):
     goal = [1, 2, 3, 4, 5, 6, 7, 8, 0]
-    return puzzle.data == goal
+    return data == goal
