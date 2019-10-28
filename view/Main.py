179e91684e10a069ddf36f51b9db2cfe2fef6b4c
Mohammad Hossein <iammhz77@gmail.com>
2019-10-28 16:52:39 +0330

    Creating Tree Image in another thread

--- view/Main.py
+++ view/Main.py
@@ -5,15 +5,38 @@ from anytree.exporter import DotExporter
 import qdarkstyle
 import threading
 import random
-from ImageProcessor.ImageProcessor import image_parser
+from ImageProcessor.ImageProcessor import image_parser, load_image, show_image
 from model.Search import Search
 from model.Puzzle8 import Puzzle8
-from multiprocessing.pool import ThreadPool
+
 from multiprocessing import Queue
 from PyQt5.QtCore import QThread, pyqtSignal
 from PyQt5 import QtWidgets
-import time
-from multiprocessing import Process
+
+
+# using this class for creating tree image
+class ImageThread(QThread):
+    signal = pyqtSignal('PyQt_PyObject')
+
+    def __init__(self, algorithm_name, tree=None):
+        QThread.__init__(self)
+        self.algorithm_name = algorithm_name
+        self.tree = tree
+
+    def set_tree(self, tree):
+        self.tree = tree
+
+    def set_algorithm_name(self, algorithm_name):
+        self.algorithm_name = algorithm_name
+
+    def run(self):
+        # checking if we have * in names
+        if self.algorithm_name[-1] == '*':
+            path = "./treeImages/" + str(self.algorithm_name[0:-1]) + " tree.png"
+        else:
+            path = "./treeImages/" + str(self.algorithm_name) + " tree.png"
+        DotExporter(self.tree).to_picture(path)
+        self.signal.emit(path)
 
 
 class AlgorithmThread(QThread):
@@ -62,17 +85,13 @@ class Window(QtWidgets.QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.search = Search()
-        self.process = None
-        self.pool = ThreadPool(processes=1)
-        self.result = []
 
         item_list = ["BFS", "DFS", "UCS", "A*", "IDS", "IDA*"]
         self.ui.comboBox.addItems(item_list)
 
         self.current_algorithm = self.ui.comboBox.currentText()
         # self.start_puzzle_date = self.get_random_list()
-        self.start_puzzle_date = [1, 2, 3, 4, 5, 6, 0, 7, 8]
+        self.start_puzzle_date = [1, 2, 3, 0, 4, 6, 7, 5, 8]
 
         # put labels together for better control
         self.labels = []
@@ -99,11 +118,18 @@ class Window(QtWidgets.QMainWindow):
         # if you don't put [str] it return the index of combobox selected item
         self.ui.comboBox.activated[str].connect(self.combobox_changed)
 
-        self.qThread = AlgorithmThread(Puzzle8(self.start_puzzle_date))
-        self.qThread.signal.connect(self.on_received)
+        # initialize threads
+        # algorithm threads
+        self.algorithm_thread = AlgorithmThread(Puzzle8(self.start_puzzle_date))
+        self.algorithm_thread.signal.connect(self.on_received)
+        # Image thread:
+        self.image_thread = ImageThread(self.current_algorithm)
+        self.image_thread.signal.connect(self.on_tree_recived)
 
     def combobox_changed(self, text):
         self.current_algorithm = text
+        # updating ui in case of ruing algorithms continuously
+        self.set_labels_text(self.start_puzzle_date)
 
     def start_randomly(self):
         random_list = self.get_random_list()
@@ -134,43 +160,40 @@ class Window(QtWidgets.QMainWindow):
         for i, label in enumerate(self.labels):
             label.setText(str(number_list[i]))
 
-    def on_received(self, item):
-        threading.Thread(target=self.display_tree, args=(item[1],)).start()
-        self.set_label_when_find_goal(item[0].state_stack)
-
     def start(self):
         puzzle = Puzzle8(self.start_puzzle_date)
         print(self.current_algorithm)
 
-        queue = Queue()
+        # Terminate threads that may run
+        self.algorithm_thread.terminate()
+        self.image_thread.terminate()
 
-        self.qThread.terminate()
-
-        self.qThread.set_algorithm_type(self.current_algorithm)
-        self.qThread.set_puzzle(puzzle)
+        self.algorithm_thread.set_algorithm_type(self.current_algorithm)
+        self.algorithm_thread.set_puzzle(puzzle)
         if self.current_algorithm == 'BFS':
-            self.qThread.start()
+            self.algorithm_thread.start()
 
         elif self.current_algorithm == 'DFS':
             # self.process = Process(target=self.search.dfs, args=(puzzle,))
             # self.process.start()
 
-            self.qThread.start()
+            self.algorithm_thread.start()
 
         elif self.current_algorithm == 'UCS':
-            self.qThread.start()
+            self.algorithm_thread.start()
 
         elif self.current_algorithm == 'A*':
-            self.qThread.start()
+            self.algorithm_thread.start()
 
         elif self.current_algorithm == 'IDS':
-            self.qThread.start()
+            self.algorithm_thread.start()
 
         elif self.current_algorithm == 'IDA*':
-            self.qThread.start()
+            self.algorithm_thread.start()
 
     def end(self):
-        self.qThread.terminate()
+        self.algorithm_thread.terminate()
+        self.image_thread.terminate()
 
     def set_label_when_find_goal(self, stack):
         for item in stack:
@@ -181,9 +204,18 @@ class Window(QtWidgets.QMainWindow):
             self.labels[index0].setText(data2)
             self.labels[index1].setText(data1)
 
-    def display_tree(self, tree):
-        # DotExporter(tree).to_dotfile("tree.dot")
-        DotExporter(tree).to_picture("tree.png")
+    def on_received(self, item):
+        tree = item[1]
+        self.image_thread.set_algorithm_name(self.current_algorithm)
+        self.image_thread.set_tree(tree)
+        self.image_thread.start()
+
+        self.set_label_when_find_goal(item[0].state_stack)
+
+    def on_tree_recived(self, path):
+
+        image = load_image(path)
+        show_image(self.current_algorithm, image, 0)
 
 
 def main():
